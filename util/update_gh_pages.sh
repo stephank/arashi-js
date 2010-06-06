23b7847f8655839fae6dca436cbe573960e4e32d
Stéphan Kochen <stephan@kochen.nl>
2010-06-06 15:41:41 +0200

    Add a small shell script to automate updating github pages.

---
+++ util/update_gh_pages.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Updates gh-pages branch with distributables from the current HEAD.
+
+if [ ! -f arashi.html ]; then
+  echo "Execute from the project root!"
+  exit 1
+fi
+
+echo "Uncommitted changes will be lost!"
+echo "Press Ctrl+C to abort, or Enter to continue."
+read dummy
+
+TEMPTAR="/tmp/arashi-gh-pages.tar"
+
+set -e
+set -x
+
+REVISION=`git rev-parse HEAD`
+
+make dist
+(
+  cd dist && \
+  tar -cf ${TEMPTAR} * && \
+  mv arashi.html index.html
+)
+
+git clean -fdx
+git checkout gh-pages
+
+tar -xf ${TEMPTAR}
+rm -f ${TEMPTAR}
+
+git add -A
+git commit -a -m "Updating from revision ${REVISION}"
+git push origin gh-pages
+
+git checkout master
