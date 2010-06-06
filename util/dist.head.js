d57a7020578227f29ea5c969d0bdb28af4ec58ce
Stéphan Kochen <stephan@kochen.nl>
2010-06-06 15:19:02 +0200

    More complete header for distributables.

--- util/dist.head.js
+++ util/dist.head.js
@@ -1,2 +1,8 @@
-/*! Project ARASHI */
+/*!
+ * ArashiJS
+ * http://github.com/stephank/arashi-js
+ *
+ * © 2010, Stéphan Kochen
+ * Licensed under the GNU GPL version 2.
+ */
 //"Engine:nomunge Snd:nomunge";
