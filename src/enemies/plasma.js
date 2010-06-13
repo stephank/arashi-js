f22862259cc107996ea662b9f9e91fdb30206a8f
Stéphan Kochen <stephan@kochen.nl>
2010-06-13 10:51:34 +0200

    Slight changes in plasma style.

--- src/enemies/plasma.js
+++ src/enemies/plasma.js
@@ -19,14 +19,14 @@ Plasma.prototype.update = function() {
 };
 
 Plasma.prototype.draw = function() {
-  var angle = this.depth * 21 + 2 * Math.PI,
+  var angle = this.depth * 63,
       i, dx, dy;
 
   c.save();
     grid.screenTranslation();
     grid.laneTranslation(this.lane, this.depth)
 
-    c.lineWidth = 0.01;
+    c.lineWidth = 0.02;
     // FIXME
     c.strokeStyle = 'white';
     c.fillStyle = 'white';
@@ -35,10 +35,10 @@ Plasma.prototype.draw = function() {
       c.moveTo(0, 0);
       for (i = 0; i < 3; i++) {
         angle += Math.PI * 2 / 3;
-        dx = Math.cos(angle) / 5;
-        dy = Math.sin(angle) / 5;
+        dx = Math.cos(angle) / 6;
+        dy = Math.sin(angle) / 6;
         c.lineTo(dx, dy);
-        c.fillRect(-(dx / 2), -(dy / 2), 0.02, 0.02);
+        c.fillRect(-(dx / 2), -(dy / 2), 0.03, 0.03);
       }
     c.stroke();
   c.restore();
