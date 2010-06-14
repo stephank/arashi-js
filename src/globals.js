c36f1a0a236667d8c39b9c149437ddae0eb148d5
Stéphan Kochen <stephan@kochen.nl>
2010-06-14 19:23:56 +0200

    Do away with Constants; only use the shorthand C.

--- src/globals.js
+++ src/globals.js
@@ -1,10 +1,8 @@
 // This file is special. Because these global variables are referenced immediatly on execution of
 // some other scripts, this should be one of the first source files included.
 
-// Constants is just a container for predefined and precalculated stuff.
-var Constants = {};
-// A shorthand.
-var C = Constants;
+// A container for constants; predefined and precalculated stuff.
+var C = {};
 
 // The canvas drawing context. (See engine.js.)
 var c = null;
@@ -15,11 +13,11 @@ var frame = null;
 var grid = null;
 
 // Radians per degree, used in conversion.
-Constants.radPerDeg = Math.PI / 180;
+C.radPerDeg = Math.PI / 180;
 
 // Constants for Z-coordinates.
 // When in normal play, the grid starts at 20 and goes down another 120.
-Constants.startDepth = 20;
-Constants.depth = 120;
-Constants.endDepth = Constants.startDepth + Constants.depth;
-Constants.fogDepth = Constants.startDepth * 2;
+C.startDepth = 20;
+C.depth = 120;
+C.endDepth = C.startDepth + C.depth;
+C.fogDepth = C.startDepth * 2;
